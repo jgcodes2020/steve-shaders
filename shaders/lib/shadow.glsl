ca09f391e119354dd16b3aa186a046693a52b626
jgcodes2020 <j349guo@uwaterloo.ca>
2025-08-04 00:03:14 -0400

    document shadow GLSL

--- shaders/lib/shadow.glsl
+++ shaders/lib/shadow.glsl
@@ -10,7 +10,7 @@ const float SHADOW_MIN_RADIUS   = 0.5;
 const float SHADOW_MAX_RADIUS   = 1.5;
 const float SHADOW_RADIUS_SCALE = 7.0;
 
-// Distorts positions in shadow space to enlarge shadows near the player
+// Distorts a position in clip space to capture more detail near the center
 vec3 shadowDistort(vec3 clipPos) {
   // XY distortion function:
   //  (a + 1) * R
@@ -31,44 +31,7 @@ vec3 shadowDistort(vec3 clipPos) {
   return clipPos;
 }
 
-// Distorts the shadow position in XY only.
-vec2 shadowDistortH(vec2 clipPos) {
-  float denom = l4norm(clipPos) + DISTORT_A;
-  return fma(clipPos, vec2(DISTORT_A), clipPos) / denom;
-}
-
-// Computes the Jacobian of the distortion function.
-// If we define the distorted coordinates as (u, v) and undistorted as (x, y),
-// this function compute (du/dx) and (dv/dy).
-mat2 distortJacobian(vec2 clipPos) {
-  // Expression for du/dx:
-  //   (a + 1) * [a * (x^4 + y^4)^(3/4) + y^4]
-  // -------------------------------------------
-  // (x^4 + y^4)^(3/4) * [a + (x^4 + y^4)^(1/4)]
-  // Expression for du/dy:
-  //              (a+1) * x * y^3
-  // -------------------------------------------
-  // (x^4 + y^4)^(3/4) * [a + (x^4 + y^4)^(1/4)]
-
-  vec2 posP2  = clipPos * clipPos;
-  vec2 posP3  = posP2 * clipPos;
-  vec2 posP4  = posP2 * posP2;
-  float sumP4 = posP4.x + posP4.y;
-
-  float sumP4Pow12 = sqrt(sumP4);
-  float sumP4Pow14 = sqrt(sumP4Pow12);
-  float sumP4Pow34 = sumP4Pow12 * sumP4Pow14;
-
-  vec2 diagNumer =
-    fma(vec2(DISTORT_A), vec2(sumP4Pow34), posP4.yx) * (DISTORT_A + 1.0);
-  vec2 offDiagNumer = posP3.yx * clipPos.xy * (DISTORT_A + 1.0);
-  float denom       = sumP4Pow34 * (DISTORT_A + sumP4Pow14);
-
-  vec2 diagEntries    = diagNumer / denom;
-  vec2 offDiagEntries = offDiagNumer / denom;
-  return mat2(diagEntries.x, offDiagEntries.y, offDiagEntries.x, diagEntries.y);
-}
-
+// Biases a position in clip space to avoid shadow acne.
 vec3 shadowBias(vec3 clipPos, vec3 worldNormal) {
   // project the normal into shadow space.
   vec3 shadowNormal =
@@ -80,6 +43,7 @@ vec3 shadowBias(vec3 clipPos, vec3 worldNormal) {
   return shadowNormal;
 }
 
+// Converts screen-space coordinates to clip space.
 vec4 screenToShadowClip(vec3 screenPos) {
   // Convert screen space to shadow view space
   vec3 ndcPos        = screenPos * 2.0 - 1.0;
@@ -91,79 +55,14 @@ vec4 screenToShadowClip(vec3 screenPos) {
   return shadowProjection * vec4(shadowViewPos, 1.0);
 }
 
-vec3 screenToShadowScreen(vec3 screenPos, vec3 normal, out vec3 clipPos) {
-  // Convert screen space to shadow view space
-  vec3 ndcPos        = screenPos * 2.0 - 1.0;
-  vec3 viewPos       = txProjective(gbufferProjectionInverse, ndcPos);
-  vec3 feetPlayerPos = txAffine(gbufferModelViewInverse, viewPos);
-  vec3 shadowViewPos = txAffine(shadowModelView, feetPlayerPos);
-
-  // Convert to shadow clip space, adjust coordinates
-  vec4 shadowClipPos = shadowProjection * vec4(shadowViewPos, 1.0);
-  shadowClipPos.xyz += shadowBias(shadowClipPos.xyz, normal);  // shadow bias
-  clipPos = shadowClipPos.xyz / shadowClipPos.w;
-
-  shadowClipPos.xyz = shadowDistort(shadowClipPos.xyz);  // shadow distortion
-
-  // Do perspective divide, convert to screen-space
-  vec3 shadowNdcPos = shadowClipPos.xyz / shadowClipPos.w;
-  return shadowNdcPos * 0.5 + 0.5;
-}
-
-vec4[4] boxKernel(vec2 texcoord, vec2 radius) {
-  vec2 fracParts = fract(texcoord * float(shadowMapResolution) - 0.5);
-
-  // For a given coordinate:
-  // left weight:   u + 0.5 + r - x
-  // right weight: -u + 0.5 + r + x
-  // then clamp to [0, 1].
-  vec4 xWeights = vec4(-0.5, +0.5, -0.5, -1.5) + radius.x +
-    vec4(vec2(-fracParts.x), vec2(fracParts.x));
-  vec4 yWeights = vec4(-0.5, +0.5, -0.5, -1.5) + radius.y +
-    vec4(vec2(-fracParts.y), vec2(fracParts.y));
-
-  xWeights = clamp(xWeights, 0.0, 1.0);
-  yWeights = clamp(yWeights, 0.0, 1.0);
-
-  return vec4[](
-    xWeights.xyyx * yWeights.wwzz, xWeights.zwwz * yWeights.wwzz,
-    xWeights.zwwz * yWeights.yyxx, xWeights.xyyx * yWeights.yyxx);
-}
-
-// Samples a shadow texture using a precomputed 4x4 kernel.
-// Kernel weights are multiplied by rcpSum.
-float texture4x4Kernel(
-  sampler2DShadow t, vec3 texcoord, vec4[4] kernel, float rcpSum) {
-  const ivec2[4] offsets =
-    ivec2[](ivec2(-1, +1), ivec2(+1, +1), ivec2(+1, -1), ivec2(-1, -1));
-  vec4 accum = vec4(0.0);
-  accum +=
-    textureGatherOffset(t, texcoord.xy, texcoord.z, offsets[0]) * kernel[0];
-  accum +=
-    textureGatherOffset(t, texcoord.xy, texcoord.z, offsets[1]) * kernel[1];
-  accum +=
-    textureGatherOffset(t, texcoord.xy, texcoord.z, offsets[2]) * kernel[2];
-  accum +=
-    textureGatherOffset(t, texcoord.xy, texcoord.z, offsets[3]) * kernel[3];
-  return dot(accum, vec4(rcpSum));
-}
-// Samples the alpha component of a texture using a precomputed 4x4 kernel.
-// Kernel weights are multiplied by rcpSum.
-float texture4x4Kernel_a(
-  sampler2D t, vec2 texcoord, vec4[4] kernel, float rcpSum) {
-  const ivec2[4] offsets =
-    ivec2[](ivec2(-1, +1), ivec2(+1, +1), ivec2(+1, -1), ivec2(-1, -1));
-  vec4 accum = vec4(0.0);
-  accum += textureGatherOffset(t, texcoord, offsets[0], 3) * kernel[0];
-  accum += textureGatherOffset(t, texcoord, offsets[1], 3) * kernel[1];
-  accum += textureGatherOffset(t, texcoord, offsets[2], 3) * kernel[2];
-  accum += textureGatherOffset(t, texcoord, offsets[3], 3) * kernel[3];
-  return dot(accum, vec4(rcpSum));
-}
-
 // SOFT SHADOWS
 // ===============================================
+// PCF with shadow distortion is by far the best way to get good-looking shadows
+// in Iris's current pipeline. Ideally we'd be using other techniques but
+// this is as good as it gets with one shadow pass.
 
+// The shadow test for opaque surfaces.
+// Takes into account the transparency of the surface but not its colour.
 float testShadow(vec3 shadowScreenPos) {
   float test   = texture(shadowtex1, shadowScreenPos);
   float tlTest = texture(shadowtex0, shadowScreenPos);
@@ -172,6 +71,7 @@ float testShadow(vec3 shadowScreenPos) {
   return max(tlTest, max(test - alpha, 0.0));
 }
 
+// Function to perform PCF over an opaque surface.
 float computeShadowSoft(vec4 shadowClipPos, vec3 normal, ivec2 pixelCoord) {
   const int sampleCount = ST_SHADOW_SAMPLES * ST_SHADOW_SAMPLES * 4;
   const float offsetMul =
@@ -200,11 +100,13 @@ float computeShadowSoft(vec4 shadowClipPos, vec3 normal, ivec2 pixelCoord) {
   return accum / float(sampleCount);
 }
 
+// The shadow test for translucent surfaces.
 float tlTestShadow(vec3 shadowScreenPos, float alpha) {
   float tlTest = texture(shadowtex0, shadowScreenPos);
   return tlTest;
 }
 
+// Function to perform PCF over a translucent surface.
 float tlComputeShadowSoft(
   vec4 shadowClipPos, float alpha, vec3 normal, ivec2 pixelCoord) {
   const int sampleCount = ST_SHADOW_SAMPLES * ST_SHADOW_SAMPLES * 4;
